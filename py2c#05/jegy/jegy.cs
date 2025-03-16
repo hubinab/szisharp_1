a6e92707b0ea5f4db59d8b7a2dc597df4cb62129
hubinab <hubinab@gmail.com>
2025-03-16 19:50:20 +0100

    py2c#05

---
+++ py2c#05/jegy/jegy.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Adja meg a megtakarított pénzét (Ft)! ");
+int Penz = Convert.ToInt32(Console.ReadLine());
+Console.Write("Mennyibe kerül egy mozijegy (Ft)? ");
+int Jegy = Convert.ToInt32(Console.ReadLine());
+
+// Az egész ellenőrzés nincs benne! A ToInt32 konverziónál elhasal, ha nem egészet adnak be.
+if (Penz <= 0 || Jegy <= 0)
+{
+    Console.WriteLine("Hibás a bemenő adat.");
+}
+else
+{
+    Console.WriteLine($"A megtakarított pénzből {Penz/Jegy} mozijegyet lehet vásárolni.");   
+}
+
