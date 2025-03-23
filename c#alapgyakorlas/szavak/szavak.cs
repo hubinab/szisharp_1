367b0f18b8084690fcc5f5e0517ed8a0ae4ebeb1
hubinab <hubinab@gmail.com>
2025-03-23 22:29:46 +0100

    250319 alap gyakorlás

---
+++ c#alapgyakorlas/szavak/szavak.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Kérem adjon meg egy szöveget: ");
+string Szoveg = Console.ReadLine()!;
+
+if (Szoveg.Length % 7 == 0)
+{
+    Console.WriteLine("A szöveg betűszáma osztható héttel!");
+}
+else
+{
+    Console.WriteLine("A szöveg betűszáma nem osztható héttel!");
+}
---
+++ c#alapgyakorlas/teglalap/teglalap.cs
@@ -0,0 +1,16 @@
+﻿double TeglalapTerulete (double a, double b)
+{
+    return Math.Round(a * b / 10000000000,12);
+}
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Kérem adja be az 'a' oldalt: ");
+    double aOldal = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Kérem adja be a 'b' oldalt: ");
+    double bOldal = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"A megadott oldalakhoz tartozó téglalamp {TeglalapTerulete(aOldal, bOldal):F12} km2 területű!");
+}
