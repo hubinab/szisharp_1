b90b39b7db93c71c282691cea80e3dff2edcd366
hubinab <hubinab@gmail.com>
2025-03-23 20:24:22 +0100

    1

--- py2c#04/masodik/obj/Debug/net8.0/masodik.AssemblyInfo.cs
+++ py2c#04/masodik/obj/Debug/net8.0/masodik.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("masodik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31dc749443d7827c6436542d78f7560c0468e330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d017424f8314965f57c791cff64d9d8a63c1a6b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("masodik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("masodik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
